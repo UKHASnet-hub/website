c01544f49fb21b297c863e16164c20f442064987
Phil Crump <phildcrump@gmail.com>
2014-01-17 11:40:10 +0000

    Add EOL to test upload log.

--- api/uploadTest.php
+++ api/uploadTest.php
@@ -1,7 +1,7 @@
 <?php
 $logFile = "../uploadLog";
 
-$line = date("D, H:i:s ").$_REQUEST['origin'].": ".$_REQUEST['data'];
+$line = date("D, H:i:s ").$_REQUEST['origin'].": ".$_REQUEST['data'].PHP_EOL;
 
 $fh = fopen($logFile, 'a') or die("Can't open file");
 fwrite($fh, $line);
