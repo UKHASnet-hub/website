82bbab68d86c17ab30421ca30ab0f18dd8dec73b
Phil Crump <phil@philcrump.co.uk>
2015-05-23 23:41:04 +0100

    Update jquery.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         dist: {
             files: {
                 'dist/static/jquerybootstrap.min.js': [
-                    'dist/static/jquery/jquery-1.11.2.min.js',
+                    'dist/static/jquery/jquery-1.11.3.min.js',
                     'dist/static/bootstrap/js/bootstrap.min.js'
                 ],
                 'dist/static/compiled.min.css': [
